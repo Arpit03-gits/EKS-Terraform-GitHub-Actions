b40d6819c799a0002502a3891a45fbaebc45d6db
Arpit Mishra <studenthub404@gmail.com>
2026-07-24 10:58:22 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "dev-arpit-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lockfile = "Lock-Files"
     encrypt        = true
   }
 }
